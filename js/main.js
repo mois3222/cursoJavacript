490449c94b6ddc3c858563c701db475e51547224
mois3222 <moisky222@gmail.com>
2023-02-02 14:09:33 -0500

    commit: Callbacks

--- js/main.js
+++ js/main.js
@@ -37,7 +37,7 @@ pddta: sigue en README.
 
 spt();
 /*Código asincrono no Bloqueante */
-
+/* 
 (() => {
   console.log(`Código Sincrono`);
   console.log(`Inicio`);
@@ -58,4 +58,32 @@ spt();
 
   uno();
   console.log(`Fin`);
-})();
+})();*/
+
+console.clear();
+
+const cuadradoCallback = (value, callback) => {
+  setTimeout(() => {
+    callback(value, value * value);
+  }, 0 | (Math.random() * 1000));
+};
+
+cuadradoCallback(0, (value, result) => {
+  console.log(`Inicia callback`);
+  console.log(`Callback: ${value}, ${result}`);
+  cuadradoCallback(1, (value, result) => {
+    console.log(`Callback: ${value}, ${result}`);
+    cuadradoCallback(2, (value, result) => {
+      console.log(`Callback: ${value}, ${result}`);
+      cuadradoCallback(3, (value, result) => {
+        console.log(`Callback: ${value}, ${result}`);
+        cuadradoCallback(4, (value, result) => {
+          console.log(`Callback: ${value}, ${result}`);
+          cuadradoCallback(5, (value, result) => {
+            console.log(`Callback: ${value}, ${result}`);
+          });
+        });
+      });
+    });
+  });
+});
